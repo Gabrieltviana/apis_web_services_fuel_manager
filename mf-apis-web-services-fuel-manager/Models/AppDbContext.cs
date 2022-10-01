1233c89fdcb4de61986af0ae7c549a0c9385364a
Gabriel T Viana <gabriel_sp2001@hotmail.com>
2022-09-30 22:29:52 -0300

    Adicionar arquivos de projeto.

---
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //AppDbContext recebendo a injeção de dependencia do DbContext options
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }   
+        //Tabelas no BD
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+    }
+}
