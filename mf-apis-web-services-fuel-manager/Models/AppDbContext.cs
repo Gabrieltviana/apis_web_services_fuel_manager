1a89c396f131586b814060c18e087b154f7e9624
Gabriel T Viana <gabriel_sp2001@hotmail.com>
2022-10-04 18:43:41 -0300

    Inserindo as rotas com os Métodos Http

--- mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
+++ mf-apis-web-services-fuel-manager/Models/AppDbContext.cs
@@ -5,9 +5,8 @@ namespace mf_apis_web_services_fuel_manager.Models
     public class AppDbContext : DbContext
     {
         //AppDbContext recebendo a injeção de dependencia do DbContext options
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }   
+        public AppDbContext(DbContextOptions options) : base(options){ }   
+        
         //Tabelas no BD
         public DbSet<Veiculo> Veiculos { get; set; }
         public DbSet<Consumo> Consumos { get; set; }
